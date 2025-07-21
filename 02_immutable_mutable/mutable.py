e24900cec7b54c0e4ea7f7aec3040de34f02d7fb
cod1ngByte <rajiv.ranjansoc@gmail.com>
2025-07-21 17:20:23 +0530

    immutable and mutable

---
+++ 02_immutable_mutable/mutable.py
@@ -0,0 +1,20 @@
+
+username  = 'debyte'
+print(username) # debyte
+
+username = 'python'
+print(username) # python
+
+
+#---------------------------------
+
+x = 10
+y = x
+print(x) # 10
+print(y) # 10
+
+x = 15
+print(x)  # 15
+print(y)  # 10
+
+#refer notion notes on immutable and mutable
